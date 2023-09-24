f0d50139c10b6dd4d85a32dda3c10910338ad749
JoaoPstein <vulgostein@gmail.com>
2023-09-24 12:16:03 -0300

    feat: start a project

---
+++ Desafio.backend/DesafioBackend/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿namespace Desafio.Backend.Entities
+{
+    public class User
+    {
+        public User(string? name, string? document, string? email, string? password)
+        {
+            Id = new Guid();
+            Name = name;
+            Document = document;
+            Email = email;
+            Password = password;
+        }
+
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Document { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
